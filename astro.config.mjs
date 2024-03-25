a4f25e426dc79247558c6b1d753fe863783cf02a
John Corser <johnpc@umich.edu>
2024-03-24 21:27:02 -0400

    remove amplify adapter (The size of the build output exceeds the max allowed size)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config"
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind"
-import awsAmplify from 'astro-aws-amplify';
 
 export default defineConfig({
   integrations: [
     tailwind(),
     icon()
   ],
-  output: "server",
-  adapter: awsAmplify(),
+  output: "static",
 })
 
